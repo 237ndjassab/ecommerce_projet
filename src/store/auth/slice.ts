fbb69b51d5ec747621180508a461c1f536afa3fe
jiresnodem <nodemjires@gmail.com>
2025-11-26 13:40:30 +0100

    feat: ajout des types pour la slice auth et initialisation du state de la slice

--- src/store/auth/slice.ts
+++ src/store/auth/slice.ts
@@ -1,11 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { AuthInfo, AuthToken } from "../../types/user";
+import type { ApiError, statusType } from "../../types/base";
 
 export interface AuthState {
-  value: number;
+  userInfo: AuthInfo | null;
+  token: AuthToken | null;
+  status: {
+    register: statusType;
+    login: statusType;
+    refresh: statusType;
+  };
+  error: {
+    register: ApiError;
+    login: ApiError;
+    refresh: ApiError;
+  };
 }
 
 const initialState: AuthState = {
-  value: 0,
+  userInfo: null,
+  token: null,
+  status: {
+    register: "idle",
+    refresh: "idle",
+    login: "idle",
+  },
+  error: {
+    register: { message: null },
+    refresh: { message: null },
+    login: { message: null },
+  },
 };
 
 export const authSlice = createSlice({
