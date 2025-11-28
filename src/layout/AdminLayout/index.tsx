5a4a411036175d7ea861b214a2b36e24217b1d73
237ndjassab <ndjassabwilly@gmail.com>
2025-11-28 11:45:25 +0100

    creation de la page Dashboard --Note: Pas encore terminer

--- src/layout/AdminLayout/index.tsx
+++ src/layout/AdminLayout/index.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "./partLeft/sidebar.js";
-import PartRightContent from "./partRight/PartRightContent.js";
+import { Outlet } from "react-router";
+import Header from "./Header.js";
+import Footer from "./Footer.js";
 
 const AdminLayout = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const handleToggleSidebar = () => {
+    setIsOpen((previousValue)=>!previousValue);
+  }
+
   return (
     <div className="flex flex-row w-full">
-      <SideBar />
-      <PartRightContent />
+      <SideBar isOpen={isOpen} />
+      <div className={`flex flex-col overflow-y-auto ${isOpen ? "w-[80%]" : "w-full"} h-screen`}>
+          <Header toggleSidebar = {handleToggleSidebar}/>
+          <div className=" w-full p-4 bg-gray-100 ">
+            <Outlet context={{isOpen}} />
+          </div>
+          <Footer/>
+         </div>
     </div>
   );
 };
--- src/store/auth/actions.ts
+++ src/store/auth/actions.ts
@@ -7,7 +7,7 @@ export const registerAction = createAsyncThunk<ApiResponse<User>, RegisterDto>(
   "auth/registerAction",
   async (data, apiThunk) => {
     try {
-      const response = await fetch("http://127.0.0.1:3000/api/auth/register", {
+      const response = await fetch("http://localhost:3000/api/auth/register", {
         method: "Post",
         headers: {
           accept: "application/json",
@@ -34,31 +34,33 @@ export const registerAction = createAsyncThunk<ApiResponse<User>, RegisterDto>(
   }
 );
 
-// export const loginAction = createAsyncThunk<ApiResponse<AuthInfo>, LoginDto>(
-//   "auth/loginAction",
-//   async (data) => {
-//     try {
-//       const response = await fetch("http://127.0.0.1:3000/api/auth/login", {
-//         method: "Post",
-//         headers: {
-//           accept: "application/json",
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(data),
-//       });
+export const loginAction = createAsyncThunk<ApiResponse<AuthInfo>, LoginDto>(
+  "auth/loginAction",
+  async (data, apiThunk) => {
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-//       if (!response.ok) {
-//         const error = await response.json();
-//         console.log("Failed to ctreate user: ", error);
-//         throw new Error("Failed to create user.");
-//       }
-//       const result = await response.json();
+      if (!response.ok) {
+        const error = await response.json();
+        console.log("Failed to verify user ", error);
+        return apiThunk.rejectWithValue("Failed to verify user");
+      }
+      const result = await response.json();
 
-//       console.log("Data login: ", result);
+      console.log("Data login: ", result);
 
-//       return data;
-//     } catch (error) {
-//       console.log("Errore on register resquest: ", error);
-//     }
-//   }
-// );
+      return result;
+    } catch (error) {
+      return apiThunk.rejectWithValue(
+        (error as {message: string}).message || "Failed to verify user."
+      );
+    }
+  }
+);
