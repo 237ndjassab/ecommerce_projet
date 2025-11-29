cf0f69e545cc79ccf84d18c114b00e64e30f66dc
237ndjassab <ndjassabwilly@gmail.com>
2025-11-29 18:50:07 +0100

    creation des pages allcategories et categoryList -Note: Pas encore terminé

--- src/layout/AdminLayout/partLeft/sidebar.tsx
+++ src/layout/AdminLayout/partLeft/sidebar.tsx
@@ -84,10 +84,10 @@ const SideBar = ({ isOpen }: Props) => {
                     <Link to={"/"}>Product</Link>
                   </li>
                   <li className="ml-7 cursor-pointer hover:text-white transition-all duration-300 ease-in-out">
-                    <Link to={"/"}>Category List</Link>
+                    <Link to={"/admin/categoryList"}>Category List</Link>
                   </li>
                   <li className="ml-7 cursor-pointer hover:text-white transition-all duration-300 ease-in-out">
-                    <Link to={"/"}>Category</Link>
+                    <Link to={"/admin/allcategory"}>Category</Link>
                   </li>
                 </motion.ul>
               )}
