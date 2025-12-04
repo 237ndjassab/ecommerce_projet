bd062a616d4fc9e89a9ba9189afe76f26f225b94
bgte237 <bgte237@gmail.com>
2025-12-04 08:37:08 +0100

    blog

--- src/pages/Main/Blogs/Blog.tsx
+++ src/pages/Main/Blogs/Blog.tsx
@@ -1,8 +1,204 @@
-import React from 'react'
+// import React from 'react';
+import { FaChevronRight } from "react-icons/fa";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+     <main>
+      <div className="flex justify-center items-center gap-2 bg-slate-200 h-10">
+        <h1 className="text-gray-500 capitalize">home</h1>
+        <FaChevronRight/>
+        <h1 className="text-gray-500 capitalize">blog</h1>
+      </div>
+      <div className=" flex flex-col-3 justify-center items-center gap-2  p-4">
+        <div className=" lg:flex lg:flex-col-2 md:flex-col-1 gap-2 ">
+          <div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="../" alt="img1" className=" hover:scale-90" />
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">sed et quam quis</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">the limited edition club des sport courchevel</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">effortlessly blends the carefree style</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">etain vulputate gravida</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">vivamus in ultrices</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">sed mattis est libero</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+          </div>
+          <div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">the variety of style and prices are endless</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">christine is A true style icon</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">aliquam efficitur dolor</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">vestibulum est justo</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">suspendisse non ex sem</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+            <div className="p-4">
+              <div className="border-2 bg-gray-300 w-150 h-100 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <h1 className="text-2xl capitalize hover:text-red-600">Aenean ut mauris</h1>
+              <p className="text-gray-500">A recent study has found that sleep aromatherapy may nataly enhance menory</p>
+              <button className="capitalize border-2 rounded-xl p-2 border-black w-40 h-10 hover:text-white hover:bg-black">read more</button>
+            </div>
+          </div>
+          <div className="">
+            <h1 className="capitalize text-2xl">Recent post</h1>
+            <div className="flex flex-row gap-2 p-2">
+
+              <div className="border-2 bg-gray-300 w-10 h-5 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+
+              <div>
+                <button className="capitalize bg-slate-50 p-1 hover:text-white hover:bg-black">news</button>
+                <h1 className="hover:text-red-600">sed et quam quis</h1>
+
+              </div>
+            </div>
+            <div className="flex flex-row gap-2 p-2">
+              <div className="border-2 bg-gray-300 w-15 h-5 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <div>
+                <button className="capitalize bg-slate-50 p-1 hover:text-white hover:bg-black">news</button>
+                <h1 className="capitalize hover:text-red-600">the variety of styles and prices are endless</h1>
+
+              </div>
+            </div>
+            <div className="flex flex-row gap-2 p-2">
+              <div className="border-2 bg-gray-300 w-15 h-5 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <div>
+                <button className="capitalize bg-slate-50 p-1 hover:text-white hover:bg-black">news</button>
+                <h1 className="capitalize hover:text-red-600">the limited edition club des sports courchevel</h1>
+
+              </div>
+            </div>
+            <div className="flex flex-row gap-2 p-2">
+              <div className="border-2 bg-gray-300 w-15 h-5 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <div>
+                <button className="capitalize bg-slate-50 p-1 hover:text-white hover:bg-black">news</button>
+                <h1 className="capitalize hover:text-red-600">christine is a true style icon</h1>
+
+              </div>
+            </div>
+            <div className="flex flex-row gap-2 p-2">
+              <div className="border-2 bg-gray-300 w-15 h-5 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <div>
+                <button className="capitalize bg-slate-50 p-1 hover:text-white hover:bg-black">news</button>
+                <h1 className="capitalize hover:text-red-600">effortlessly blends the carefree style</h1>
+
+              </div>
+            </div>
+            <div className="flex flex-row gap-2 p-2 ">
+              <div className="border-2 bg-gray-300 w-10 h-5 border-gray-300">
+                <img src="." alt="img1" className="hover:scale-90" />
+
+              </div>
+              <div>
+                <button className="capitalize bg-slate-50 p-1 hover:text-white hover:bg-black">news</button>
+                <h1 className="capitalize hover:text-red-600">aliquam efficitur dolor</h1>
+
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+
+      </div>
+    </main>
   )
 }
 
