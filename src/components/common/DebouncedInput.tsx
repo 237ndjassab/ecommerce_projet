74fb33ec2228b9abb92a483f296187d942f96954
237ndjassab <ndjassabwilly@gmail.com>
2025-12-15 12:49:01 +0100

    implementation de l'option de filtre a la home page -Note: PAS ENCORE TERMINé

--- src/components/common/DebouncedInput.tsx
+++ src/components/common/DebouncedInput.tsx
@@ -1,30 +1,40 @@
-import React from 'react'
+import React from "react";
 
 type Props = {
-    value: string;
-    onChange: (value: string) => void;
-    delay?: number;
-} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>;
+  value: string;
+  onChange: (value: string) => void;
+  delay?: number;
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "value" | "onChange">;
 
-const DebouncedInput:React.FC<Props> = ({value, onChange, delay, ...props}) => {
-    const [displayValue, setDisplayValue] = React.useState(value);
-    
-    React.useEffect(() => {
-        const handler = setTimeout(() => {
-            if (displayValue !== value) {
-                onChange(displayValue);
-            }
-        }, delay || 500);
-        return () => {
-            clearTimeout(handler);
-        };
-    }, [displayValue, onChange, delay, value]);
+const DebouncedInput: React.FC<Props> = ({
+  value,
+  onChange,
+  delay,
+  ...props
+}) => {
+  const [displayValue, setDisplayValue] = React.useState(value);
+
+  React.useEffect(() => {
+    const handler = setTimeout(() => {
+      if (displayValue !== value) {
+        onChange(displayValue);
+      }
+    }, delay || 500);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [displayValue, onChange, delay, value]);
 
   return (
     <div>
-        <input {...props} type="text" onChange={(e)=> setDisplayValue(e.target.value)} />
+      <input
+        {...props}
+        type="text"
+        className="placeholder:text-gray-400 outline-0 px-2.5 w-full"
+        onChange={(e) => setDisplayValue(e.target.value)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default DebouncedInput
+export default DebouncedInput;
