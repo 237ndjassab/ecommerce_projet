fb2eaea9180df61e2bf4bb3da2447089d7b5ad0b
237ndjassab <ndjassabwilly@gmail.com>
2026-03-23 15:04:32 +0100

    ajout de chat

--- src/components/chat/MessageInput.tsx
+++ src/components/chat/MessageInput.tsx
@@ -1,10 +1,39 @@
 import { useState } from "react";
+import Utils from "../../helpers/Utils";
+import useAppSelector from "../../hooks/useAppSelector";
+import { socket } from "../../services/socket";
+import type { Message } from "../../pages/Main/types.chat.ts";
 
-export default function MessageInput() {
-  // const user = useAppSelector((state) => state.auth.userInfo);
-  // const id = user?.user.id;
+type MessageInputProps = {
+  setMessages: React.Dispatch<React.SetStateAction<Message[]>>;
+};
+
+export default function MessageInput({ setMessages }: MessageInputProps) {
+  const user = useAppSelector((state) => state.auth.userInfo);
+  const senderId = user?.user.id;
+
   const [message, setMessage] = useState("");
+  const conversationId = Utils.getConversationId();
 
+  const handleSendMsg = () => {
+    if (!message.trim()) return;
+
+    const newMessage: Message = {
+      content: message,
+      senderId,
+    };
+
+    // 🔥 affichage immédiat (optimistic UI)
+    setMessages((prev) => [...prev, newMessage]);
+
+    socket.emit("sendMessage", {
+      senderId,
+      conversationId,
+      message,
+    });
+
+    setMessage("");
+  };
 
   return (
     <div className="flex items-center border-t border-gray-200 bg-gray-50 p-3">
@@ -15,8 +44,9 @@ export default function MessageInput() {
         placeholder="Tape ton message..."
         className="flex-1 rounded-full border border-gray-300 px-4 py-2 text-sm focus:border-pink-500 focus:ring focus:ring-pink-200 outline-none transition"
       />
+
       <button
-        // onClick={sendMessage}
+        onClick={()=>handleSendMsg()}
         className="ml-3 rounded-full bg-pink-600 px-5 py-2 text-sm font-semibold text-white shadow hover:bg-pink-700 transition-colors"
       >
         Envoyer
--- src/components/chat/MessageList.tsx
+++ src/components/chat/MessageList.tsx
@@ -1,37 +1,42 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { socket } from "../../services/socket";
+import type { Message } from "../../pages/Main/types.chat.ts";
+import useAppSelector from "../../hooks/useAppSelector.ts";
 
-type Message = {
-  id?: number;
-  content: string;
-  senderId: number;
+type MessageListProps = {
+  messages: Message[];
+  setMessages: React.Dispatch<React.SetStateAction<Message[]>>;
 };
 
-export default function MessageList() {
-  const [messages, setMessages] = useState<Message[]>([]);
-
+export default function MessageList({
+  messages,
+  setMessages,
+}: MessageListProps) {
+  const user = useAppSelector((state) => state.auth.userInfo);
+  const ActiveUser = user?.user.id;
   useEffect(() => {
     socket.on("newMessage", (message: Message) => {
+      console.log("newMessage arrived : ", message);
       setMessages((prev) => [...prev, message]);
     });
 
     return () => {
       socket.off("newMessage");
     };
-  }, []);
+  }, [setMessages]);
 
   return (
-    <div className="flex-1 p-4 overflow-y-auto bg-gray-50">
+    <div className="flex-1 w-full p-4 overflow-y-auto bg-gray-50">
       {messages.map((msg, index) => (
         <div
           key={index}
-          className={`mb-3 flex ${
-            msg.senderId === 1 ? "justify-end" : "justify-start"
+          className={`mb-3 flex w-full ${
+            msg.senderId === ActiveUser ? "justify-start" : "justify-end"
           }`}
         >
           <div
             className={`max-w-xs rounded-lg px-4 py-2 text-sm shadow ${
-              msg.senderId === 1
+              msg.senderId === ActiveUser
                 ? "bg-pink-600 text-white"
                 : "bg-white text-gray-800 border border-gray-200"
             }`}
